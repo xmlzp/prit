e65d10d38890af338138bab34937045adb3c7101
1609162771@qq.com <1609162771@qq.com>
2019-09-12 18:49:25 +0800

    再次修改

--- files/pages/position-detail/position-detail.js
+++ files/pages/position-detail/position-detail.js
@@ -9,11 +9,9 @@ Page({
 		detali :[] ,//数据的详情
 		windowHeight:'' //屏幕的高度
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-<<<<<<< HEAD
   onLoad: function (options) {
 		//获取屏幕的高度
 		var windowHeight = wx.getSystemInfoSync().windowHeight
@@ -41,91 +39,37 @@ Page({
 		              })
 		         },
 		 });
-=======
-  onLoad: function(options) {
-    //跳转详情页面
->>>>>>> b32f1cd317fd7090d1acf1dc8a002f73945654fc
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
-
+	}
   /**
    * 立即报名
    */
-  sign: function() {;
-    wx.getStorage({
-      key: 'phone',
-      success(res) {
-        console.log(res.data);
-         if(res.data =="" || res.data == null){
-           wx.showModal({
-             title: '提示',
-             content: '尊敬的用户，你未登录，请登录后立即报名',
-             confirmText: "确定",
-             showCancel: false,
-             success: function (res) {
-               if (res.confirm) {
-                 wx.navigateTo({
-                   url: '../login/login',
-                 })
-               }
-             }
-           })
-         } else {
-           wx.showToast({
-             title: '报名成功',
-             icon: 'success',
-             duration: 2000,
-           })
-         }
-      }
-    })
-  }
+  // sign: function() {;
+  //   wx.getStorage({
+  //     key: 'phone',
+  //     success(res) {
+  //       console.log(res.data);
+  //        if(res.data =="" || res.data == null){
+  //          wx.showModal({
+  //            title: '提示',
+  //            content: '尊敬的用户，你未登录，请登录后立即报名',
+  //            confirmText: "确定",
+  //            showCancel: false,
+  //            success: function (res) {
+  //              if (res.confirm) {
+  //                wx.navigateTo({
+  //                  url: '../login/login',
+  //                })
+  //              }
+  //            }
+  //          })
+  //        } else {
+  //          wx.showToast({
+  //            title: '报名成功',
+  //            icon: 'success',
+  //            duration: 2000,
+  //          })
+  //        }
+  //     }
+  //   })
+  // }
 })
